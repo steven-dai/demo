ed1f9c1d5a48aef301677493c23117ccd1f054e7
StevenDai <dai_shuyang@qq.com>
2024-01-04 22:39:08 +0800

    增加百度活体检测

--- face/src/main/java/site/daishuyang/face/FaceController.java
+++ face/src/main/java/site/daishuyang/face/FaceController.java
@@ -1,5 +1,7 @@
 package site.daishuyang.face;
 
+import cn.hutool.core.codec.Base64Encoder;
+import cn.hutool.core.io.IoUtil;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson.JSONArray;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -30,26 +35,47 @@ public class FaceController {
 
     @Value("${ztuo.baidu.clientSecret}")
     private String clientSecret;
-    
-    @RequestMapping(value = "verify",method = RequestMethod.POST)
-    public String verify(@RequestBody FaceParam faceParam){
+
+    @RequestMapping(value = "check", method = RequestMethod.POST)
+    public String check(@RequestBody FaceParam faceParam) {
+        // 请求url
+        String url = "https://aip.baidubce.com/rest/2.0/face/v1/faceliveness/verify";
+        try {
+            // 设置jsonBody参数
+            Map<String, Object> param = new HashMap<>();
+            param.put("video_base64", convertMP4ToBase64(faceParam.getVideo()));
+            // 注意这里仅为了简化编码每一次请求都去获取access_token，线上环境access_token有过期时间， 客户端可自行缓存，过期后重新获取。
+            String accessToken = getAuth();
+            url = url + "?access_token=" + accessToken;
+            String result = HttpUtil.post(url, param);
+            JSONObject resultJson = JSONObject.parseObject(result);
+            System.out.println(resultJson);
+            return resultJson.toJSONString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Fail";
+    }
+
+    @RequestMapping(value = "verify", method = RequestMethod.POST)
+    public String verify(@RequestBody FaceParam faceParam) {
         // 请求url
         String url = "https://aip.baidubce.com/rest/2.0/face/v3/match";
         try {
             // 设置jsonBody参数
             JSONArray array = new JSONArray();
             JSONObject param1 = new JSONObject();
-            param1.put("image",faceParam.getImage1());
-            param1.put("image_type","URL");
+            param1.put("image", faceParam.getImage1());
+            param1.put("image_type", "URL");
             array.add(param1);
             JSONObject param2 = new JSONObject();
-            param2.put("image",faceParam.getImage2());
-            param2.put("image_type","URL");
+            param2.put("image", faceParam.getImage2());
+            param2.put("image_type", "URL");
             array.add(param2);
             String jsonBody = JSONUtil.toJsonStr(array);
             // 注意这里仅为了简化编码每一次请求都去获取access_token，线上环境access_token有过期时间， 客户端可自行缓存，过期后重新获取。
             String accessToken = getAuth();
-            url = url+"?access_token="+accessToken;
+            url = url + "?access_token=" + accessToken;
             System.out.println(url);
             System.out.println(jsonBody);
             String result = HttpUtil.post(url, jsonBody);
@@ -61,9 +87,10 @@ public class FaceController {
         }
         return "Fail";
     }
-    
+
     /**
      * 获取权限token
+     *
      * @return 返回示例：
      * {
      * "access_token": "24.460da4889caad24cccdb1fea17221975.2592000.1491995545.282335-1234567",
@@ -73,10 +100,11 @@ public class FaceController {
     public String getAuth() {
         return getAuth(clientId, clientSecret);
     }
-    
+
     /**
      * 获取API访问token
      * 该token有一定的有效期，需要自行管理，当失效时需重新获取.
+     *
      * @param ak - 百度云官网获取的 API Key
      * @param sk - 百度云官网获取的 Securet Key
      * @return assess_token 示例：
@@ -122,5 +150,19 @@ public class FaceController {
         }
         return null;
     }
-    
+
+    private static String convertMP4ToBase64(String mp4Url) throws IOException {
+        // 从URL读取MP4文件的InputStream
+        InputStream inputStream = new URL(mp4Url).openStream();
+        try {
+            // 将InputStream转为字节数组
+            byte[] mp4Bytes = IoUtil.readBytes(inputStream);
+            // 将字节数组编码为Base64字符串
+            return Base64Encoder.encode(mp4Bytes);
+        } finally {
+            // 关闭输入流
+            IoUtil.close(inputStream);
+        }
+    }
+
 }
--- face/src/main/java/site/daishuyang/face/FaceParam.java
+++ face/src/main/java/site/daishuyang/face/FaceParam.java
@@ -5,24 +5,34 @@ package site.daishuyang.face;
  * @date 2021-08-06 02:11
  */
 public class FaceParam {
-    
+
     private String image1;
-    
+
     private String image2;
-    
+
+    private String video;
+
     public String getImage1() {
         return image1;
     }
-    
+
     public void setImage1(String image1) {
         this.image1 = image1;
     }
-    
+
     public String getImage2() {
         return image2;
     }
-    
+
     public void setImage2(String image2) {
         this.image2 = image2;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
     }
 }
